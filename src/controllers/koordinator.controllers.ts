de4a22f506ca8681990d350228f9748c7b0d7870
Gilang Indra <gilangindra734@gmail.com>
2024-12-11 23:27:00 +0700

    ⚡ fix – delete some code in controller and route koordinator

--- src/controllers/koordinator.controllers.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { PrismaClient } from '@prisma/client';
-import { validationResult } from 'express-validator';
-import fs from 'fs';
-import path from 'path';
-
-const prisma = new PrismaClient();
-
